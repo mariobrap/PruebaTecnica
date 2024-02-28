0fe18599e3546a8aecd5108ca49c698cbded46a0
Mario Braga <mariobrap@gmail.com>
2024-02-28 00:35:38 -0600

    fix: port env

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
       }
     }) 
   );
-  await app.listen(process.env.PORT);
+  dotenv.config();
+  await app.listen(Number(process.env.PORT) || 3005);
 }
 bootstrap();
